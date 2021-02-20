aded95faac9bf553e6e3feb522a939c2ecf4e735
thedaughterofdragon <74292218+thedaughterofdragon@users.noreply.github.com>
2021-02-20 13:02:22 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,48 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling1=loadImage('sprites/sling1.png')
+        this.sling2=loadImage('sprites/sling2.png')
+        this.sling3=loadImage('sprites/sling3.png')
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.sling1,200,20)
+         image(this.sling2,170,20)
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            if (pointA.x<220) {strokeWeight(6);
+                stroke(48,22,8)
+                line(pointA.x, pointA.y, pointB.x, pointB.y);
+                line(pointA.x, pointA.y, pointB.x +50, pointB.y);
+                 image(this.sling3,pointA.x-30,pointA.y-10,15,25)   
+               
+            }
+            else{
+
+                strokeWeight(2);
+            stroke(48,22,8)
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x, pointA.y, pointB.x +50, pointB.y);
+             image(this.sling3,pointA.x-30,pointA.y-10,15,25)   
+            }
+                       pop();
+        }
+    }
+    
+}
